b7d2e4a72dd7e58346fcf752e6183c2b0a9e76db
Sean Duggan <sean.duggan@gmail.com>
2024-07-22 09:46:40 -0400

    Current UI work

---
+++ Assets/StandardCanvas.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class StandardCanvas : MonoBehaviour
+{
+    [SerializeField] Canvas helpScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowHelpScreen()
+    {
+
+    }
+
+    public void HideHelpScreen()
+    {
+
+    }
+}
